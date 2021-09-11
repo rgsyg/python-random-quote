bad602d127d68500d377ae040ba085aa1ee4e06c
RGR <rigorsyguatbsn@gmail.com>
2021-09-11 23:32:24 +0800

    I uncommented line 2

--- get-quote.py
+++ get-quote.py
@@ -9,8 +9,10 @@ def primary():
 
   last = 13
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], end='')
+  print(quotes[rnd2], end='')
 
 if __name__== "__main__":
   primary()
